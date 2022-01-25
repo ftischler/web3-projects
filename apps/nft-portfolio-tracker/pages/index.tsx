3972699f56603583d2cdc5babc3a0beeecf7716e
Florian Tischler <Florian.Tischler@gmx.net>
2022-01-25 23:43:12 +0100

    feat(nft-portfolio-tracker): hello page

--- apps/nft-portfolio-tracker/pages/index.tsx
+++ apps/nft-portfolio-tracker/pages/index.tsx
@@ -5,6 +5,7 @@ export function Index(): ReactElement {
   return (
     <div className={styles.page}>
       <h1>Treasure Portfolio Tracker</h1>
+      <p>Available s00n. l0l.</p>
     </div>
   );
 }
